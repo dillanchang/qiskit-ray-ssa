613b843b3c3f841458236e7096301ae77cd2b62c
gdjomani265 <gdjomani265@ucla.edu>
2021-08-07 20:37:05 -0400

    I added main by gilles after losing the original file

--- src/main_by_Gilles.py
+++ src/main_by_Gilles.py
@@ -1,11 +1,17 @@
+#This code creates Actual Error vs Predicted Error plots 
+#of some given parameters for a gridsearch
+#edited by Gilles Djomani on August 5, 2021
+
 import orbit_prediction.spacetrack_etl as etl
 import orbit_prediction.build_training_data as training
 import orbit_prediction.ml_model as ml
 import sklearn.metrics as metrics
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import xlsxwriter
 import pickle
+
 
 from sklearn.svm import *
 from sklearn.svm import LinearSVR
@@ -18,94 +24,81 @@ from sklearn.model_selection import GridSearchCV, cross_val_score
 from sklearn.model_selection import StratifiedShuffleSplit
 from numpy.random import randint
 
+def main(C, kernel, degree, epsilon, gamma):
+    train_test_data = pickle.load(open("train_test_data.pkl", "rb"))  
+    if kernel == 'linear':
+        SVR_svm = LinearSVR(C=C, epsilon=epsilon, tol=0.00001, loss='epsilon_insensitive', 
+            fit_intercept=True, intercept_scaling=1.0, dual=True, verbose=0, 
+            random_state=None, max_iter=100)
+    else:
+        SVR_svm= SVR(C=C, kernel=kernel, epsilon=epsilon, gamma=gamma,
+                 tol=0.00001, shrinking=True, cache_size=7000, verbose=False, max_iter=100)
+    svm_reg = MultiOutputRegressor(SVR_svm)
 
-def rmse(C, kernel, degree, epsilon, gamma):
-    train_test_data = pickle.load(open("train_test_data.pkl", "rb"))
-    rbf_svm= SVR(C=C, kernel=kernel, epsilon=epsilon, gamma=gamma,
-             tol=0.0000001, shrinking=True, cache_size=200, verbose=False, max_iter=-1)
-    svm_reg = MultiOutputRegressor(rbf_svm)
-    print("*** now fitting the x_train and y_train data ***")
-    svm_reg.fit(train_test_data['X_train'], train_test_data['y_train'])
-    lin_y_hat = svm_reg.predict(train_test_data['X_test'])
-    rmse = metrics.mean_squared_error(train_test_data['y_test'], lin_y_hat,
-                                           squared=False, multioutput='raw_values')
-    print("*** now returning the rmse values ***")
-    return rmse
+    svm_reg.fit(train_test_data['X_train'], train_test_data['y_train'])         
+    lin_y_hat = svm_reg.predict(train_test_data['X_test'])                      #22 x 6
+    rmse = metrics.mean_squared_error(train_test_data['y_test'], lin_y_hat,     
+                                squared=False, multioutput='raw_values')
 
-def main(vary_name, C, kernel, degree, epsilon, gamma, file_name):
-    print(vary_name, C, kernel, degree, epsilon, gamma, file_name)
-    kernel=kernel
-    names = [vary_name,'error_r_x','error_r_y','error_r_z',        #name of each column error percent
-                 'error_v_x','error_v_y','error_v_z']
-    #We want to replace the following optional array parameters with if statements
-    #that automatically assigns our defined array parameters to be n
-    #n = [1, 10, 100, 1000, 10000]          #n exist in {C}
-    #n = [1e-2, 1e-4, 1e-8, 1e-16, 1e-32]   #n exist in {gamma}
-    #n = [2, 3, 4, 5, 6, 7, 8, 9, 10,       #n exist in {degree}
-    #   13, 17, 19, 23, 29, 31]           
-    #n = [0.1, 0.01, 0.001, 0.0001, 0.00001]#n exist in {epsilon}
-    #n = [1, 10, 100, 1000, 10000]          #n exist in {sigma}
-    C_vary = False
-    degree_vary = False
-    epsilon_vary = False
-    gamma_vary = False
+    file_name="SVR(C="+str(C)+"_kernel="+str(kernel)+"_degree="+str(degree)+"_\u03B5="+str(epsilon)+"_\u03B3="+str(gamma)+")"
 
-    if vary_name == 'C':
-        n=C
-        C_vary=True
-    if vary_name == 'degree':
-        n=degree
-        degree_vary=True
-    if vary_name == 'epsilon':
-        n=epsilon
-        epsilon_vary=True
-    if vary_name == 'gamma':
-        n=gamm
-        gamma_vary=True
+    x_axis_for_r_x = []
+    y_axis_for_r_x = []
+    x_axis_for_r_y = []
+    y_axis_for_r_y = []
+    x_axis_for_r_z = []
+    y_axis_for_r_z = []
 
-    #save the data into an excel file
-    workbook = xlsxwriter.Workbook(file_name) #open existing excel file
-    worksheet = workbook.add_worksheet()                 #create a worksheet
+    y_axis_data = np.array(lin_y_hat)
+    y_axis_data = y_axis_data.tolist()
+
+    for j in range(0, len(y_axis_data)):
+        y_axis_for_r_x.append((y_axis_data[j][0]/(2*6.378e6))*10000)
+        y_axis_for_r_y.append((y_axis_data[j][1]/(2*6.378e6))*10000)
+        y_axis_for_r_z.append((y_axis_data[j][2]/(2*6.378e6))*10000)
+
+    for i in range(0,len(y_axis_data)):
+        x_axis_for_r_x.append((np.array(train_test_data['y_test']['physics_err_r_x'])[i]/(2*6.378e6))*10000)
+        x_axis_for_r_y.append((np.array(train_test_data['y_test']['physics_err_r_y'])[i]/(2*6.378e6))*10000)
+        x_axis_for_r_z.append((np.array(train_test_data['y_test']['physics_err_r_z'])[i]/(2*6.378e6))*10000)
+
+    max_ = 0 
+    if max_ < max(x_axis_for_r_x):
+        max_ = max(x_axis_for_r_x)
+    if max_ < max(x_axis_for_r_x):
+        max_ = max(y_axis_for_r_x)
+
+    x_axis_for_dashed_line = np.linspace(-max_,max_,20)
+    y_axis_for_dashed_line = x_axis_for_dashed_line
+
+    fig, ax = plt.subplots()
+    plt.title(file_name)
     
-    #initialize the rows and columns of the table
-    row = 0
-    col = 0
-    worksheet.write(row, col, "SVR( C="+str(C)+", kernel="+str(kernel)+ 
-        ", degree="+str(degree)+", epsilon="+str(epsilon)+ 
-        ", tol=0.0000001, shrinking=True, cache_size=200, verbose=False, max_iter=-1)")
-    row = 1
-    #place all names along the zero-th row
-    for name in range(0,len(names)):
-        worksheet.write(row, col, names[name])
-        col += 1
-    #place n_pred_days input into corresponding row and col
-    col = 0
-    row = 2
+    plt.plot(x_axis_for_dashed_line, y_axis_for_dashed_line, '--', color='red')
+    plt.plot(x_axis_for_r_x, y_axis_for_r_x, 'o', color='blue', label='r_x')
+    plt.plot(x_axis_for_r_y, y_axis_for_r_y, 'o', color='purple', label='r_y')
+    plt.plot(x_axis_for_r_z, y_axis_for_r_z, 'o', color='green', label='r_z')
+    plt.xlabel("Actual Error (km)")
+    plt.ylabel("Predicted Error (km)")
+    plt.xlim([-max_, max_])
+    plt.xlim([-max_, max_])
+    ax.legend()
+    plt.savefig(kernel+'/'+file_name+'.jpg')
 
-    for i in range(0,len(n)):
-        worksheet.write(row, col, n[i])
-        row += 1
-    #place x and v error data into row and col
-    row = 2
-    for i in range(0,len(n)):
-        col = 1
-        r_m_s_e = np.array(rmse(C=n[i] if C_vary==True else C, kernel=kernel, 
-            degree=n[i] if degree_vary==True else degree, 
-            epsilon=n[i] if epsilon_vary==True else epsilon,
-            gamma=n[i] if gamma_vary==True else gamma))
-        for j in range(0,len(r_m_s_e)):
-            worksheet.write(row, col, r_m_s_e[j])
-            col += 1
-        row += 1
-    #close workbook
-    workbook.close()
+    print("\n*** rmse data for ", file_name, "is listed below ***")
+    print(rmse)
 if __name__ == "__main__":
-    main(vary_name='C', C=[1, 10, 100, 1000, 10000], 
-        kernel='poly', degree=3, epsilon=0.1, gamma=None, file_name='src/rmse_data.xlsx')
-    #vary_name is a string name of the varying parameter (e.g. 'C', 'degree', 'gamma')
-    #kernel is a string (e.g. 'linear', 'poly', 'rbf'')
-    #C is a number or 1d array containing varying Constants
-    #degree is a number or 1d array containg varying degree values
-    #epsilon is a number or 1d array of containing varying epsilon values
-    #gamma is a number of 1d array containing varing gamma values
-
+    #create gridsearch parameters
+    vary1 = [10, 500,3000, 7500]         #C
+    vary2 = [3, 8, 13, 24]               #degree 
+    vary3 = [1e+5, 1e-2, 1e-5, 1e-8]     #epsilon
+    vary4 = [1e+5, 'scale', 1e-5, 1e+8]  #gamma
+    
+    #create plots from the gridsearch
+    for i in range(0,len(vary1)):
+        for j in range(0,len(vary1)):
+            for k in range(0,len(vary1)):
+                for l in range(0,len(vary1)):
+                    main(C=vary1[i], kernel='linear', degree=vary2[j], epsilon=vary3[k], gamma=vary4[l])
+    
+    #main(C=3000, kernel='linear', degree=24, epsilon=1e-8, gamma='scale')
--- src/train_test_data.py
+++ src/train_test_data.py
@@ -11,13 +11,13 @@ spacetrack_client = etl.build_space_track_client(SPACETRACK_USERNAME,
                                                  SPACETRACK_PASSWORD )
 spacetrack_etl = etl.SpaceTrackETL(spacetrack_client)
 iss_orbit_data = spacetrack_etl.build_leo_df( norad_ids=['25544'],
-                                              last_n_days=183,
+                                              last_n_days=365,
                                               only_latest=None )
 ## Creating model and test data
 physics_model_predicted_orbits = training.predict_orbits(iss_orbit_data,
                                 last_n_days=None, n_pred_days=1)
 physics_model_errors = training.calc_physics_error(physics_model_predicted_orbits)
-train_test_data = ml.build_train_test_sets(physics_model_errors, test_size=0.2)
+train_test_data = ml.build_train_test_sets(physics_model_errors, test_size=0.25)
 #print("shape of train_test_data:", train_test_data.size)
 
 empty_list = []
